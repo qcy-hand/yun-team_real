b25c33840745b2163d827e54c5a5d8f529034cb5
qcy-hand <1024809717@qq.com>
2019-06-03 02:32:17 +0800

    nine commit

--- miniprogram/pages/search/page_search.js
+++ miniprogram/pages/search/page_search.js
@@ -1,13 +1,48 @@
 // miniprogram/pages/search/page_search.js
-import Toast from '/vant-weapp/dist/toast';
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    array: ['拼车', '运动', '学习', '自定义'],
+    objectArray: [{
+        id: 0,
+        name: '拼车'
+      },
+      {
+        id: 1,
+        name: '运动'
+      },
+      {
+        id: 2,
+        name: '学习'
+      },
+      {
+        id: 3,
+        name: '自定义'
+      }
+    ],
+    Choose: 0,
   },
+
+  //自带选择器传值
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      Choose: e.detail.value
+    })
+  },
+
+  //搜索内容传值
+  onSearch(res) {
+    this.setData({
+      Sercontent: res.event.detail
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
